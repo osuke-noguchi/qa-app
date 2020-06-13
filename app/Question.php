93b249c1b5d92db23575809826f905f6aa68e985
osuke-noguchi <nogcuhi.nodai@gmail.com>
2020-06-13 02:40:42 +0000

    Add votes, answers and views counter to question item

--- app/Question.php
+++ app/Question.php
@@ -28,6 +28,17 @@ class Question extends Model
 
     public function getCreatedDateAttribute()
     {
-       return $this->created_at->diffForHumans();
+        return $this->created_at->diffForHumans();
+    }
+
+    public function getStatusAttribute()
+    {
+        if ($this->answers > 0) {
+            if ($this->best_answer_id) {
+                return "answered_accepted";
+            }
+            return "answered";
+        }
+        return "unanswered";
     }
 }
