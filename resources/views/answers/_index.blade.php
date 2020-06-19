6a3569a463ebff69cecd4bfc4c784560e62743c3
osuke-noguchi <noguchi.nodai@gmail.com>
2020-06-19 22:42:11 +0900

    Implemnt DRY principle on the application views

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -32,39 +32,7 @@
                                 @csrf
                                 <input type="hidden" name="vote" value="-1">
                             </form>
-                            <a title="Click to mark as favorite question (Click again to undo)"
-                            class="favorite mt-2 {{ Auth::guest() ? 'off' : ($question->is_favorited ? 'favorited' : '') }}"
-                            onclick="event.preventDefault(); document.getElementById('favorite-question-{{ $question->id }}').submit();"
-                            >
-                                <i class="fa fa-star fa-2x"></i>
-                                <span class="favorites-count">
-                                    {{ $question->favorites_count }}
-                                </span>
-                            </a>
-                            <form id ="favorite-question-{{ $question->id }}" action="/questions/{{ $question->id }}/favorites" method="POST" style="display:none;">
-                                @csrf
-                                @if ($question->is_favorited)
-                                    @method('DELETE')
-                                @endif
-                            </form>
-                            @can ('accept', $answer)
-                                <a title="Mark this answer as best answer"
-                                    class="{{ $answer->status }} mt-2"
-                                    onclick="event.preventDefault(); document.getElementById('accept-answer-{{ $answer->id }}').submit();"
-                                    >
-                                    <i class="fa fa-check fa-2x"></i>
-                                </a>
-                                <form id ="accept-answer-{{ $answer->id }}" action="{{ route('answers.accept', $answer->id) }}" method="POST" style="display:none;">
-                                    @csrf
-                                </form>
-                            @else
-                                @if ($answer->is_best)
-                                    <a title="The question owner accepted this answer as best answer"
-                                        class="{{ $answer->status }} mt-2">
-                                        <i class="fa fa-check fa-2x"></i>
-                                    </a>
-                                @endif
-                            @endcan
+
                         </div>
                         <div class="media-body">
                             {!! $answer->body_html !!}
@@ -89,19 +57,10 @@
                                 </div>
                                 <div class="col-4"></div>
                                 <div class="col-4">
-                                    <span class="text-muted">
-                                        Answered {{ $answer->created_date }}
-                                    </span>
-                                    <div class="media mt-2">
-                                        <a href="{{ $answer->user->url }}" class="pr-2">
-                                            <img src="{{ $answer->user->avatar }}">
-                                        </a>
-                                        <div class="media-body mt-1">
-                                            <a href="{{ $answer->user->url }}">
-                                                {{ $answer->user->name }}
-                                            </a>
-                                        </div>
-                                    </div>
+                                    @include ('shared._author', [
+                                        'model' => $answer,
+                                        'label' => 'answer'
+                                    ])
                                 </div>
                             </div>
                         </div>
